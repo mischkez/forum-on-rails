48fda294098279e949712f45a917fc4325b89b15
Milos Jankovic <milos.jankovic@wikala.com>
2019-11-24 20:36:03 +0100

    added rich text editor to question body + slug generation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,8 @@ require('@fortawesome/fontawesome-free/js/all');
 require('jquery');
 require('popper.js');
 require('bootstrap');
-
+require("trix");
+require("@rails/actiontext");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,12 +3,20 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :replies
 
+  after_validation :generate_slug
+
+  has_rich_text :body
+
   paginates_per 20
-  
+
   default_scope { includes(:user, :category) }
   scope :with_replies, -> { includes(:replies) }
 
+  def generate_slug
+    self.slug = title.parameterize
+  end
+
   def to_param
-    slug 
+    slug
   end
 end
